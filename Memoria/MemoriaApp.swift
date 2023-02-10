3f2ea5523dee519f80efc7323f8a88a73fa3cdb9
Erick <erickweil2@gmail.com>
2023-02-10 16:59:05 -0400

    Initial Commit

---
+++ Memoria/MemoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriaApp.swift
+//  Memoria
+//
+//  Created by Erick Leonardo Weil on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
